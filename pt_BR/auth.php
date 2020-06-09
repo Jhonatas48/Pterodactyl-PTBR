bd158632b036705c9f2efe7c7080fdd79c0ebb64
Filipe Rocha <filipesilva.tmsi@gmail.com>
2020-06-09 03:53:50 -0300

    Commit inicial

---
+++ pt_BR/auth.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'not_authorized' => 'Você não está autorizado a realizar esta ação.',
+    'auth_error' => 'Ocorreu um erro ao tentar fazer login.',
+    'authentication_required' => 'Autenticação é requerida para continuar.',
+    'remember_me' => 'Lembrar de mim',
+    'sign_in' => 'Login',
+    'forgot_password' => 'Eu esqueci minha senha!',
+    'request_reset_text' => 'Esqueceu a senha da sua conta? Isso não é o fim do mundo, insira seu email abaixo.',
+    'reset_password_text' => 'Redefina a senha da sua conta.',
+    'reset_password' => 'Redefinir senha da conta',
+    'email_sent' => 'Um email foi enviado para você com instruções para redefinir a sua senha.',
+    'failed' => 'As credenciais fornecidas não correspondem às que temos registradas, ou o token 2FA fornecido era inválido.',
+    'throttle' => 'Muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+    'password_requirements' => 'As senhas devem conter pelo menos um caractere maiúsculo, um minúsculo e um numérico e devem ter pelo menos 8 caracteres.',
+    'request_reset' => 'Localizar conta',
+    '2fa_required' => 'Autenticação por 2-Fatores (2FA)',
+    '2fa_failed' => 'O token 2FA usado é inválido.',
+    'totp_failed' => 'Ocorreu um erro ao tentar validar o TOTP.',
+    '2fa_must_be_enabled' => 'O administrador solicitou que a autenticação de 2-Fatores seja ativada em sua conta para usar o painel.',
+];
